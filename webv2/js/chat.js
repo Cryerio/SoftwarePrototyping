c6ab42bd7adaa7f15fe6d5673869f1463c9db84b
Jingwen Ye <jingwen.ye@stud.uni-hannover.de>
2020-01-12 15:18:06 +0100

    add live chat

---
+++ webv2/js/chat.js
@@ -0,0 +1,14 @@
+$(function() {
+    $('#sendbutton').click(function() {
+        var divfield = document.createElement("div");
+        divfield.classList ="d-flex justify-content-end mb-4";
+        var divcontext = document.createElement("div");
+        divcontext.classList =  "msg_cotainer_send";
+        divcontext.innerHTML=$('#msgfield').val();
+        divfield.appendChild(divcontext);
+        document.getElementById("msg_field").appendChild(divfield);
+        $('#msgfield').val("");
+    });
+
+});
+
